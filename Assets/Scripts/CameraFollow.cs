075353e71fb976238095018ebc0bc677f6e80d21
II3xx <b24vikgr@student.his.se>
2024-11-30 03:03:07 +0100

    NewMapDrafts+CameraChanges

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -20,12 +20,12 @@ public class CameraFollow : MonoBehaviour
   {
     Vector3 targetPosition = target.position + offset;
     targetPosition.z = transform.position.z;
-    
+
     transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothing);
 
-    if (bounds)
+    /*if (bounds)
     {
       transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x), Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
-    }
+    }*/
   }
 }
