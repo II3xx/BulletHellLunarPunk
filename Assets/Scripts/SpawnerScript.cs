f45359e3d95e14e8ff93e81866b595ce62eb0b8a
II3xx <b24vikgr@student.his.se>
2024-12-08 20:24:07 +0100

    WorkedOnSpawnersFurther

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-
+using UnityEngine.InputSystem.Controls;
 
 
 public class SpawnerScript : MonoBehaviour
 {
     public GameObject spawnedObject;
-    private int i;
+    private float i = 8;
+    public int spawnTime;
     private int spawnedCount;
     public int maxSpawnCount;
+    public GameObject spawnerInstance;
+   
     
     // Start is called before the first frame update
     void Start()
@@ -23,20 +26,20 @@ public class SpawnerScript : MonoBehaviour
     void Update()
     {
         spawnedCount = GameObject.FindGameObjectsWithTag("Spawner").Length; 
-        if (spawnedCount <= maxSpawnCount && i >= 1000) 
+        if (spawnedCount < maxSpawnCount && i >= spawnTime) 
         {
             SpawnObject();
             i = 0;
         }
         else
         {
-            i++;
+            i += Time.deltaTime;
         }
        
     }
 
     void SpawnObject()
     {
-        Instantiate(spawnedObject, spawnedObject.transform);
+        Instantiate(spawnedObject, new Vector3()  { x = spawnerInstance.transform.position.x-1,  y= spawnerInstance.transform.position.y, z = spawnerInstance.transform.position.z}, Quaternion.identity);
     }
 }
