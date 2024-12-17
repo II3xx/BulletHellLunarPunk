8d8aaed2efd5eb25d23e2bfe228773713d43111e
Elphea <a24fregr@student.his.se>
2024-12-17 12:51:56 +0100

    Update 2 2024-12-17

--- Assets/Scripts/DialogueHolder.cs
+++ Assets/Scripts/DialogueHolder.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu(fileName = "Dialogue", menuName = "Dialogue/DialogueText", order = 1)]
 public class DialogueHolder : ScriptableObject
 {
     [SerializeField] private List<string> DialogueText;
     [SerializeField] private bool isRunic;
+    private int current;
 
     public bool IsRunic
     {
         get => IsRunic;
     }
 
-    public string nextString(int current)
+    public string nextString()
     {
-        return DialogueText[current++];
+        string dialogeuToReturn = DialogueText[current];
+        current++;
+        return dialogeuToReturn;
     }
 }
