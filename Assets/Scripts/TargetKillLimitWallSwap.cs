1e8ebf941d27e1b85fd5a9950584107062350a26
Elphea <a24fregr@student.his.se>
2024-12-18 18:14:39 +0100

    Update 2 2024-12-18
    Target kill limit wall swap has virt cam shake och audio queue

--- Assets/Scripts/TargetKillLimitWallSwap.cs
+++ Assets/Scripts/TargetKillLimitWallSwap.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class TargetKillLimitWallSwap : MonoBehaviour
 {
     [SerializeField] Collider2D ToggleCollider;
     [SerializeField] [Range(0,10)] int TotalKills = 0;
+    [SerializeField] [Range(0.1f, 0.8f)] float shakeAmount = 0.1f;
+    [SerializeField] CinemachineVirtualCamera virtCam;
+    [SerializeField] AudioClip audioClip;
 
     private void Awake()
     {
@@ -19,7 +23,23 @@ public class TargetKillLimitWallSwap : MonoBehaviour
         if(TotalKills <= 0)
         {
             ToggleCollider.isTrigger = true;
+            if(virtCam != null)
+                StartCoroutine(OnKillCompletion());
+            if(audioClip != null)
+                gameObject.AddComponent<TemporaryAudioSource>().setClipAndPlay(audioClip);
         }
     }
 
+    IEnumerator OnKillCompletion()
+    {
+        float shaker = shakeAmount;
+        CinemachineBasicMultiChannelPerlin virtCamNoise = virtCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+        for (;shaker>0; shaker=-Time.deltaTime)
+        {
+            virtCamNoise.m_AmplitudeGain = shaker;
+            yield return null;
+        }
+        yield break;
+    }
 }
